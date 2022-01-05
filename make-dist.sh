92a055c7448b1532f76c5fc9d3b8ac52b6082fe5
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2022-01-04 21:24:37 -0600

    check in all the stuff.

--- make-dist.sh
+++ make-dist.sh
@@ -6,12 +6,24 @@ CP="cpmcp -f adam $DSK"
 
 rm -f $DSK
 cp $BLANK $DSK
+set -x
 $CP finfo/finfo.com 0:finfo.com
 $CP feject/feject.com 0:feject.com
 $CP fld/fld.com 0:fld.com
 $CP flh/flh.com 0:flh.com
 $CP fmount/fmount.com 0:fmount.com
 $CP fnew/fnew.com 0:fnew.com
+$CP fwifi/fwifi.com 0:fwifi.com
+$CP fhost/fhost.com 0:fhost.com
+$CP ncd/ncd.com 0:ncd.com
+$CP ndir/ndir.com 0:ndir.com
+$CP nera/nera.com 0:nera.com
+$CP nget/nget.com 0:nget.com
+$CP nlogin/nlogin.com 0:nlogin.com
+$CP nmkdir/nmkdir.com 0:nmkdir.com
+$CP nput/nput.com 0:nput.com
+$CP npwd/npwd.com 0:npwd.com
+$CP nren/nren.com 0:nren.com
 $CP README.md 0:README.TXT
-$CP LICENSE 0:LICENSE.TXT
+set +x
 
---
+++ npwd/src/network_get_prefix.c
@@ -0,0 +1,26 @@
+/**
+ * Read from channel 
+ *
+ * @param buf Buffer
+ * @param len length
+ * @return adamnet status code
+ */
+
+#include <string.h>
+#include <conio.h>
+#include "network.h"
+
+extern unsigned char response[1024];
+
+char *network_get_prefix(void)
+{
+  DCB *dcb = find_dcb();
+  char c='0'; // 0x30 (PWD)
+  unsigned char r=adamnet_write(&c,1);
+
+  if (r!=0x80)
+    return NULL;
+
+  r=adamnet_read(response,sizeof(response));
+  return response;
+}
